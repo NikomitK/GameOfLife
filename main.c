5c397b79071770393437b28930bc0564056f5a02
Niko <nikobo2003@gmail.com>
2023-02-10 10:46:03 +0100

    automatically create arrays

--- main.c
+++ main.c
@@ -5,34 +5,35 @@
 #define BOARDWIDTH 5
 #define BOARDHEIGHT 5
 
-#define MOD(a,b) ((((a)%(b))+(b))%(b))
+#define MOD(a, b) ((((a)%(b))+(b))%(b))
 
-void createBoard(char*[BOARDHEIGHT][BOARDWIDTH]);
+void createBoards();
 
 char board[BOARDHEIGHT][BOARDWIDTH];
-
+char boardZwei[BOARDHEIGHT][BOARDWIDTH];
 
 void printBoard();
 
 int checkNeighbours(int, int);
 
 void cycle();
+
 /*
  * checkNeigh gibt x um eine zelle herum zurück
  * in cycle je nach inhalt und neighbours zelle in neuem board verwalten
  * altes board durch neues ersetzen
  */
 int main() {
-    createBoard(board);
+    createBoards();
     printBoard();
     //anfangsmuster ausgeben
 
     //auswahl muster oder eigenes
 
-        //wenn eigenes, schleife mit koordinatenabfrage
+    //wenn eigenes, schleife mit koordinatenabfrage
 
 
-    while(1) {
+    while (1) {
         checkNeighbours(1, 0);
         cycle();
         printBoard();
@@ -44,54 +45,48 @@ int main() {
 
 }
 
-void createBoard(char* emptyboard[BOARDHEIGHT][BOARDWIDTH]){
-    for(int i = 0; i<BOARDHEIGHT; i++){
-        for(int j = 0; j<BOARDWIDTH; j++){
-            *emptyboard[i][j] = ' ';
+void createBoards() {
+    for (int i = 0; i < BOARDHEIGHT; i++) {
+        for (int j = 0; j < BOARDWIDTH; j++) {
+            board[i][j] = ' ';
+            boardZwei[i][j] = ' ';
         }
     }
 }
 
-void cycle(){
-    char boardZwei[BOARDHEIGHT][BOARDWIDTH] = {
-            {'X', 'X', ' ', ' ', 'X'},
-            {'X', 'X', ' ', ' ', 'X'},
-            {'X', ' ', ' ', ' ', ' '},
-            {'X', ' ', ' ', ' ', 'X'},
-            {'X', 'X', ' ', ' ', 'X'}};
+void cycle() {
 
+    for (int i = 0; i < BOARDHEIGHT; i++) {
+        for (int j = 0; j < BOARDWIDTH; j++) {
 
-    for(int i = 0; i < BOARDHEIGHT; i++){
-        for(int j = 0; j < BOARDWIDTH; j++){
+            int neigh = checkNeighbours(i, j);
 
-                int neigh = checkNeighbours(i, j);
+            if (board[i][j] == 'X') {
+                if (neigh == 2 || neigh == 3) boardZwei[i][j] = 'X';
+                else boardZwei[i][j] = ' ';
+            }
 
-                if(board[i][j] == 'X'){
-                    if(neigh == 2 || neigh == 3) boardZwei[i][j] = 'X';
-                    else boardZwei[i][j] = ' ';
-                }
-
-                if(board[i][j] == ' '){
-                    if(neigh == 3) boardZwei[i][j] = 'X';
-                    else boardZwei[i][j] = ' ';
-                }
+            if (board[i][j] == ' ') {
+                if (neigh == 3) boardZwei[i][j] = 'X';
+                else boardZwei[i][j] = ' ';
             }
         }
+    }
 
-    for(int i = 0; i < BOARDHEIGHT; i++){
-        for(int j = 0; j < BOARDWIDTH; j++) {
+    for (int i = 0; i < BOARDHEIGHT; i++) {
+        for (int j = 0; j < BOARDWIDTH; j++) {
 
             board[i][j] = boardZwei[i][j];
         }
     }
 }
 
-int checkNeighbours(int row, int col){
+int checkNeighbours(int row, int col) {
 
     int x_count = 0;
 
-    for(int i = row-1; i <= row+1; i++) {
-        for (int j = col-1; j <= col+1; j++) {
+    for (int i = row - 1; i <= row + 1; i++) {
+        for (int j = col - 1; j <= col + 1; j++) {
             if (i == row && j == col) continue;
             if (board[MOD(i, BOARDHEIGHT)][MOD(j, BOARDWIDTH)] == 'X') x_count += 1;
 
@@ -102,9 +97,8 @@ int checkNeighbours(int row, int col){
 }
 
 
-
-void printBoard(){
-    for(int i = 0; i < BOARDHEIGHT; i++) {
+void printBoard() {
+    for (int i = 0; i < BOARDHEIGHT; i++) {
         printf("| ");
         for (int j = 0; j < BOARDWIDTH; j++) {
             printf("%c | ", board[i][j]);
